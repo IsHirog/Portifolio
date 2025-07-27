0956ef48ed90fc637f990d5e9c1e5d847093aa2a
IsHirog <chritiangabriel3d@gmail.com>
2025-07-27 16:38:57 -0300

    Ajuste do scroll nos modal

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -1,18 +1,24 @@
 function abrirModal(id) {
   const modal = document.getElementById(id);
-  if (modal) modal.style.display = "block";
+  if (modal) {
+    modal.style.display = "block";
+    document.body.classList.add("modal-open");
+  }
 }
 
 function fecharModal(id) {
   const modal = document.getElementById(id);
-  if (modal) modal.style.display = "none";
+  if (modal) {
+    modal.style.display = "none";
+    document.body.classList.remove("modal-open");
+  }
 }
 
 window.onclick = function (event) {
   const modais = document.querySelectorAll(".modal");
   modais.forEach(modal => {
     if (event.target === modal) {
-      modal.style.display = "none";
+      fecharModal(modal.id);
     }
   });
 };
